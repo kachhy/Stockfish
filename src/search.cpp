7f8d1d2376d1bf6156cea959d52704a85630e42e
kachhy <109984629+kachhy@users.noreply.github.com>
2023-09-08 11:24:23 -0400

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -780,9 +780,10 @@ namespace {
         &&  eval >= beta
         &&  eval < 24923) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
-    // At 1 ply after the root node, we can prune out moves that decrease 2ply eval significantly (for example 1. g4).
+
+    // Step 8b. At 1 ply after the root node, we can prune out moves that decrease 2ply eval significantly (for example 1. g4).
     // This is possible due to the strength of the neural network.
-    else if (ss->ply == 2
+    if (ss->ply == 2
         && !improving
         && !ss->inCheck
         && eval < beta
